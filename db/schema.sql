c06da4ca5dcd2117a2508aa872bff9ac423a6e48
Jiyoh Macbook <geovannih514@gmail.com>
2023-08-30 13:06:05 -0700

    seeded DBs now rendering w/ prompts

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,8 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
@@ -21,5 +22,40 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NULL
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
+
+INSERT INTO departments (name) 
+VALUES  ('Sales'),
+        ('Marketing'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal'),
+        ('Human Resources');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('Sales Lead', 100000, 1),
+        ('Salesperson', 80000, 1),
+        ('Lead Engineer', 150000, 3),
+        ('Software Engineer', 120000, 3),
+        ('Account Manager', 160000, 4),
+        ('Accountant', 125000, 4),
+        ('Legal Team Lead', 250000, 5),
+        ('Lawyer', 190000, 5),
+        ('Lead HR', 250000, 6),
+        ('HR Representative', 150000, 6);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ('Eldon', 'Tyrel', 1, NULL),
+        ('JF', 'Sebastion', 2, 1),
+        ('Roy', 'Batty', 3, NULL),
+        ('Pris', 'Stratton', 4, 3),
+        ('Leon', 'Kowalski', 4, 3),
+        ('Rachael', 'Rosen', 5, 3),
+        ('Harry', 'Bryant', 6, NULL),
+        ('Rick', 'Deckard', 7, 6),
+        ('Gaff', 'Gaff', 8, 6),
+        ('Bryant', 'Bryant', 9, 6),
+        ('Iris', 'Iris', 10, 6);
